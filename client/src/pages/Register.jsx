15d11af095d8099f98e7c6dee955c542fdb25f58
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2023-09-09 10:36:57 +0530

    Setting up Primary Login and Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,31 @@
-import {Link} from 'react-router-dom'
+import {FormRow, Logo} from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register Layout</h1>
-            <Link to={'/login'}>Login Page</Link>
-        </div>
-    )
-}
+        <Wrapper>
+            <form className='form'>
+                <Logo />
+                <h4>Register</h4>
+                <FormRow type='text' name='name' defaultValue='John' />
+                <FormRow type='text' name='lastName' labelText='last name' defaultValue='Doe' />
+                <FormRow type='text' name='location' />
+                <FormRow type='email' name='email' />
+                <FormRow type='password' name='password' />
+
+                <button type='submit' className='btn btn-block'>
+                    submit
+                </button>
+                <p>
+                    Already a member?
+                    <Link to='/login' className='member-btn'>
+                        Login
+                    </Link>
+                </p>
+            </form>
+        </Wrapper>
+    );
+};
 
 export default Register;
